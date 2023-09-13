53e6e1a46131793083686bac063a99ca63c0c649
Jose Angel Campos Monsivais <55610980+JoseCamposM@users.noreply.github.com>
2023-09-12 21:47:47 -0600

    as

---
+++ razorpages2/Pages/formulario.cshtml.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class formularioModel : PageModel
+    {
+        public string? producto;
+        public int cantidad;
+        public double precio, total;
+
+        public IActionResult OnePost()
+        {
+
+            producto = Request.Form["producto"];
+            cantidad = int.Parse(Request.Form["cantidad"]!);
+            precio = double.Parse(Request.Form["precio"]!);
+
+            total = cantidad * precio;
+
+            return Page();
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
